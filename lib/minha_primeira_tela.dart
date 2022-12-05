86659607cb02233fa5aedf761dc82cd9be258b4e
Matheus Victor <matheusvictor.salles@gmail.com>
2022-12-05 08:35:17 -0300

    comment my first flutter page

---
+++ lib/minha_primeira_tela.dart
@@ -0,0 +1,100 @@
+/*
+ Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Stack(
+                    alignment: AlignmentDirectional.center,
+                    // diferente do Container, o Stack pode ter diversas "filhos" (children)
+                    children: [
+                      Container(
+                        color: Colors.black,
+                        width: 100,
+                        height: 100,
+                      ),
+                      Container(
+                        color: Colors.red,
+                        width: 80,
+                        height: 80,
+                      ),
+                      Container(
+                        color: Colors.blue,
+                        width: 60,
+                        height: 60,
+                      ),
+                      Container(
+                        color: Colors.yellow,
+                        width: 40,
+                        height: 40,
+                      )
+                    ],
+                  ),
+                  Stack(
+                    alignment: AlignmentDirectional.center,
+                    // diferente do Container, o Stack pode ter diversas "filhos" (children)
+                    children: [
+                      Container(
+                        color: Colors.black,
+                        width: 100,
+                        height: 100,
+                      ),
+                      Container(
+                        color: Colors.red,
+                        width: 80,
+                        height: 80,
+                      ),
+                      Container(
+                        color: Colors.blue,
+                        width: 60,
+                        height: 60,
+                      ),
+                      Container(
+                        color: Colors.yellow,
+                        width: 40,
+                        height: 40,
+                      )
+                    ],
+                  ),
+                  Stack(
+                    alignment: AlignmentDirectional.center,
+                    // diferente do Container, o Stack pode ter diversas "filhos" (children)
+                    children: [
+                      Container(
+                        color: Colors.black,
+                        width: 100,
+                        height: 100,
+                      ),
+                      Container(
+                        color: Colors.red,
+                        width: 80,
+                        height: 80,
+                      ),
+                      Container(
+                        color: Colors.blue,
+                        width: 60,
+                        height: 60,
+                      ),
+                      Container(
+                        color: Colors.yellow,
+                        width: 40,
+                        height: 40,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print('O botão foi clicado!');
+                  },
+                  child: Text('Meu botão'))
+            ],
+          ),
+        )
+ */
