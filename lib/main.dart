3b007a573c6a1e55e9e28423fbaab32eac8f4de7
Matheus Victor <matheusvictor.salles@gmail.com>
2022-12-16 11:28:18 -0300

    feat: apply radius decoration to task's card

--- lib/main.dart
+++ lib/main.dart
@@ -55,120 +55,128 @@ class _TaskState extends State<Task> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        child: Stack(
-          children: [
-            Container(
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
               color: Colors.blue,
-              height: 140,
+              borderRadius: BorderRadius.circular(4),
             ),
-            Column(
-              children: [
-                Container(
+            height: 140,
+          ),
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
                   color: Colors.white,
-                  height: 100,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        color: Colors.grey,
-                        width: 72,
-                        height: 100,
-                        child:
-                            Image.network(widget.taskImage, fit: BoxFit.cover),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            width: 200,
-                            child: Text(
-                              widget.taskName,
-                              style: const TextStyle(
-                                  fontSize: 18,
-                                  overflow: TextOverflow.ellipsis),
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.difficulty >= 1)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.difficulty >= 2)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.difficulty >= 3)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.difficulty >= 4)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.difficulty >= 5)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                      ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              level++;
-                            });
-                          },
-                          child: Icon(Icons.arrow_drop_up))
-                    ],
-                  ),
                 ),
-                Row(
+                height: 100,
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        width: 200,
-                        child: LinearProgressIndicator(
-                          color: Colors.white,
-                          value: (widget.difficulty) > 0
-                              ? (level / widget.difficulty) / 10
-                              : 1,
-                        ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: Colors.grey,
                       ),
+                      width: 72,
+                      height: 100,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(widget.taskImage,
+                              fit: BoxFit.cover)),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        'Level: $level',
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                      ),
-                    )
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 200,
+                          child: Text(
+                            widget.taskName,
+                            style: const TextStyle(
+                                fontSize: 18, overflow: TextOverflow.ellipsis),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 1)
+                                  ? Colors.blue
+                                  : Colors.blue[100],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 2)
+                                  ? Colors.blue
+                                  : Colors.blue[100],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 3)
+                                  ? Colors.blue
+                                  : Colors.blue[100],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 4)
+                                  ? Colors.blue
+                                  : Colors.blue[100],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 5)
+                                  ? Colors.blue
+                                  : Colors.blue[100],
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            level++;
+                          });
+                        },
+                        child: Icon(Icons.arrow_drop_up))
                   ],
                 ),
-              ],
-            )
-          ],
-        ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 200,
+                      child: LinearProgressIndicator(
+                        color: Colors.white,
+                        value: (widget.difficulty) > 0
+                            ? (level / widget.difficulty) / 10
+                            : 1,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      'Level: $level',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
