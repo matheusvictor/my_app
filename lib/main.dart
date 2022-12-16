de83c4610eda8d172095d5aecfb0b10210bbc838
Matheus Victor <matheusvictor.salles@gmail.com>
2022-12-16 12:14:48 -0300

    refactor: Difficulty class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:estudos_flutter/Difficulty.dart';
 
 void main() {
   runApp(MyApp());
@@ -123,45 +124,7 @@ class _TaskState extends State<Task> {
                                 fontSize: 18, overflow: TextOverflow.ellipsis),
                           ),
                         ),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.star,
-                              size: 15,
-                              color: (widget.difficulty >= 1)
-                                  ? Colors.blue
-                                  : Colors.blue[100],
-                            ),
-                            Icon(
-                              Icons.star,
-                              size: 15,
-                              color: (widget.difficulty >= 2)
-                                  ? Colors.blue
-                                  : Colors.blue[100],
-                            ),
-                            Icon(
-                              Icons.star,
-                              size: 15,
-                              color: (widget.difficulty >= 3)
-                                  ? Colors.blue
-                                  : Colors.blue[100],
-                            ),
-                            Icon(
-                              Icons.star,
-                              size: 15,
-                              color: (widget.difficulty >= 4)
-                                  ? Colors.blue
-                                  : Colors.blue[100],
-                            ),
-                            Icon(
-                              Icons.star,
-                              size: 15,
-                              color: (widget.difficulty >= 5)
-                                  ? Colors.blue
-                                  : Colors.blue[100],
-                            ),
-                          ],
-                        )
+                        Difficulty(difficultyLevel: widget.difficulty)
                       ],
                     ),
                     ElevatedButton(
