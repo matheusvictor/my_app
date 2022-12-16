ad322311db598815c21dd615b75c686d4b8a9caa
Matheus Victor <matheusvictor.salles@gmail.com>
2022-12-16 13:15:58 -0300

    refactor: rename file

---
+++ lib/task.dart
@@ -0,0 +1,114 @@
+import 'package:estudos_flutter/difficulty.dart';
+import 'package:flutter/material.dart';
+
+class Task extends StatefulWidget {
+  final String taskName;
+  final String taskImage;
+  final int difficulty;
+
+  const Task(this.taskName,
+      {Key? key,
+      this.taskImage =
+          'https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80',
+      required this.difficulty})
+      : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int level = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(4),
+            ),
+            height: 140,
+          ),
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Colors.white,
+                ),
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: Colors.grey,
+                      ),
+                      width: 72,
+                      height: 100,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(widget.taskImage,
+                              fit: BoxFit.cover)),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 200,
+                          child: Text(
+                            widget.taskName,
+                            style: const TextStyle(
+                                fontSize: 18, overflow: TextOverflow.ellipsis),
+                          ),
+                        ),
+                        Difficulty(difficultyLevel: widget.difficulty)
+                      ],
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            level++;
+                          });
+                        },
+                        child: Icon(Icons.arrow_drop_up))
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 200,
+                      child: LinearProgressIndicator(
+                        color: Colors.white,
+                        value: (widget.difficulty) > 0
+                            ? (level / widget.difficulty) / 10
+                            : 1,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      'Level: $level',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
