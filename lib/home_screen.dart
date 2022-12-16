4c89a69f70c87cd59262ae46d730900da0d61a61
Matheus Victor <matheusvictor.salles@gmail.com>
2022-12-16 13:25:34 -0300

    refactor: organize responsibilities

---
+++ lib/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:estudos_flutter/task.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool showTask = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Tasks'),
+        leading: Container(),
+      ),
+      body: AnimatedOpacity(
+        opacity: showTask ? 1 : 0,
+        duration: const Duration(milliseconds: 500),
+        child: ListView(
+          children: [
+            Task('Learn Flutter', difficulty: 5),
+            Task('Medidate',
+                difficulty: 5,
+                taskImage:
+                    'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
+            Task('Cycling',
+                difficulty: 2,
+                taskImage:
+                    'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg')
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            showTask = !showTask;
+          });
+        },
+        child: Icon(showTask == true ? Icons.visibility_off : Icons.visibility),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:estudos_flutter/Difficulty.dart';
+import 'package:estudos_flutter/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,8 +13,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool showTask = true;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,149 +20,6 @@ class _MyAppState extends State<MyApp> {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('My Tasks'),
-            leading: Container(),
-          ),
-          body: AnimatedOpacity(
-            opacity: showTask ? 1 : 0,
-            duration: const Duration(milliseconds: 500),
-            child: ListView(
-              children: [
-                Task('Learn Flutter', difficulty: 5),
-                Task('Medidate',
-                    difficulty: 5,
-                    taskImage:
-                        'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
-                Task('Cycling',
-                    difficulty: 2,
-                    taskImage:
-                        'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg')
-              ],
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                showTask = !showTask;
-              });
-            },
-            child: Icon(
-                showTask == true ? Icons.visibility_off : Icons.visibility),
-          ),
-        ));
-  }
-}
-
-class Task extends StatefulWidget {
-  final String taskName;
-  final String taskImage;
-  final int difficulty;
-
-  const Task(this.taskName,
-      {Key? key,
-      this.taskImage =
-          'https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80',
-      required this.difficulty})
-      : super(key: key);
-
-  @override
-  State<Task> createState() => _TaskState();
-}
-
-class _TaskState extends State<Task> {
-  int level = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(4),
-            ),
-            height: 140,
-          ),
-          Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(4),
-                  color: Colors.white,
-                ),
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(4),
-                        color: Colors.grey,
-                      ),
-                      width: 72,
-                      height: 100,
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(4),
-                          child: Image.network(widget.taskImage,
-                              fit: BoxFit.cover)),
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          width: 200,
-                          child: Text(
-                            widget.taskName,
-                            style: const TextStyle(
-                                fontSize: 18, overflow: TextOverflow.ellipsis),
-                          ),
-                        ),
-                        Difficulty(difficultyLevel: widget.difficulty)
-                      ],
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            level++;
-                          });
-                        },
-                        child: Icon(Icons.arrow_drop_up))
-                  ],
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: 200,
-                      child: LinearProgressIndicator(
-                        color: Colors.white,
-                        value: (widget.difficulty) > 0
-                            ? (level / widget.difficulty) / 10
-                            : 1,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Text(
-                      'Level: $level',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                  )
-                ],
-              ),
-            ],
-          )
-        ],
-      ),
-    );
+        home: HomeScreen());
   }
 }
